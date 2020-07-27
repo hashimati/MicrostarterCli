b48fd6614e5edb7b5a652a22bca5185933946b76
Ahmed al Hashmi <Hashimat@localhost.localdomain>
2020-07-28 00:54:36 +0300

    Dump Project information to the file

--- MicroCli/src/main/java/io/hashimati/utils/ConfigurationInitializer.java
+++ MicroCli/src/main/java/io/hashimati/utils/ConfigurationInitializer.java
@@ -3,6 +3,7 @@ package io.hashimati.utils;
 import de.codeshelf.consoleui.prompt.InputResult;
 import de.codeshelf.consoleui.prompt.ListResult;
 import io.hashimati.domains.ConfigurationInfo;
+import io.hashimati.domains.ProjectInfo;
 import lombok.Data;
 
 import java.io.File;
@@ -13,11 +14,14 @@ public class ConfigurationInitializer {
 
     public static  ConfigurationInfo configurationInfo;
 
+    public static ProjectInfo projectInfo;
     private ProjectValidator projectValidator = new ProjectValidator();
     public void init() throws IOException {
 
       //  Runtime.getRuntime().addShutdownHook(new Thread(()->{}));
 
+        projectInfo
+                =  projectValidator.getProjectInfo();
         if(!ProjectValidator.isValidProject())
         {
             System.out.println("The current directory is not a directory of Micronaut proejct");
@@ -48,16 +52,35 @@ public class ConfigurationInitializer {
             if(!configurationInfo.getDatabaseType().equalsIgnoreCase("mongodb")) {
                 ListResult databaseBackend = PromptGui.createListPrompt("databaseBackend", "Select Database Backend: ", "JDBC", "JPA");
                 configurationInfo.setDataBackendRun(databaseBackend.getSelectedId());
+                switch (databaseBackend.getSelectedId())
+                {
+                    case "JPA":
+                        projectInfo.getFeatures().add("data-jpa");
+
+                        break;
+                    case "JDBC":
+                        projectInfo.getFeatures().add("data-jdbc");
+                        break;
+                }
+                projectInfo.dumpToFile();
+
+                //todo add dependencies to build file;
+
             }
-            else
+            else {
                 configurationInfo.setDataBackendRun("none");
+                projectInfo.getFeatures().add("mongo-reactive");
+                //todo add dependencies to build files;
+            }
 
         }
+        projectInfo.getFeatures().add("openapi");
+        projectInfo.dumpToFile();
+        //todo add dependencies to build files.
 
+        configurationInfo.setProjectInfo(projectInfo);
 
-        configurationInfo.setProjectInfo(ProjectValidator.getProjectInfo());
-
-        System.out.println(configurationInfo);
+        //System.out.println(configurationInfo);
         configurationInfo.writeToFile();
 
     }
