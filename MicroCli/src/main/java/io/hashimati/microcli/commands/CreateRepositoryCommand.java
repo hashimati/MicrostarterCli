f0ac8f9d2e1cd43af36f26c02bb18f48a40ebf6b
Ahmed Al Hashmi <hashimati.ahmed@gmail.com>
2020-08-29 23:04:11 +0300

    Create CreateRepositoryCommand.java
    Create Repository Command

---
+++ MicroCli/src/main/java/io/hashimati/microcli/commands/CreateRepositoryCommand.java
@@ -0,0 +1,10 @@
+package io.hashimati.microcli.commands;
+
+import java.util.concurrent.Callable;
+
+public class CreateRepositoryCommand implements Callable<Integer> {
+    @Override
+    public Integer call() throws Exception {
+        return null;
+    }
+}
