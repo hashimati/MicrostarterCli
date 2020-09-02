262229e434e7491e8f4e725a039364f2a9208ccb
Ahmed Al Hashmi <hashimati.ahmed@gmail.com>
2020-09-03 01:27:45 +0300

    Update CreateControllerCommand.java

--- MicroCli/src/main/java/io/hashimati/microcli/commands/CreateControllerCommand.java
+++ MicroCli/src/main/java/io/hashimati/microcli/commands/CreateControllerCommand.java
@@ -5,6 +5,8 @@ import io.hashimati.microcli.domains.ConfigurationInfo;
 import io.hashimati.microcli.services.MicronautComponentGenerator;
 import io.hashimati.microcli.utils.GeneratorUtils;
 import io.hashimati.microcli.utils.PromptGui;
+import org.fusesource.jansi.Ansi;
+import org.fusesource.jansi.AnsiConsole;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 
@@ -23,8 +25,9 @@ public class CreateControllerCommand implements Callable<Integer> {
     @Override
     public Integer call() throws Exception {
 
+        AnsiConsole.systemInstall();
         ConfigurationInfo configurationInfo = ConfigurationInfo.fromFile(new File(ConfigurationInfo.getConfigurationFileName()) );
-        String packageName = PromptGui.inputText("package", "Enter the controller's package: ", "com.example").getInput();
+        String packageName = PromptGui.inputText("pack", "Enter the controller's package: ", configurationInfo.getProjectInfo().getDefaultPackage()).getInput();
         String className = PromptGui.inputText("className", "Enter controller name: ", "MyController").getInput();
 
         String path = PromptGui.inputText("map", "Enter the controller's path: ", "/"+className).getInput();
@@ -32,14 +35,17 @@ public class CreateControllerCommand implements Callable<Integer> {
         String content = micronautComponentGenerator.generateController(packageName, className, path,lang);
 
 
+
         String extension =GeneratorUtils.getSourceFileExtension(lang);
         String controllerPath = GeneratorUtils.generateFromTemplate(ProjectConstants.PathsTemplate.CONTROLLER_PATH, new HashMap<String, String>(){{
             put("lang", configurationInfo.getProjectInfo().getSourceLanguage());
             put("defaultPackage", GeneratorUtils.packageToPath(packageName));
         }});
+
         GeneratorUtils.createFile(System.getProperty("user.dir")+controllerPath+ "/"+className+extension, content);
 
-
+        PromptGui.printlnSuccess(className + " is create successfully!");
+        PromptGui.println("", Ansi.Color.WHITE);
 
         return null;
     }
