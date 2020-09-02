a22c4cac03c5267902a3736a174266c81493bf53
Ahmed Al Hashmi <hashimati.ahmed@gmail.com>
2020-09-03 01:27:56 +0300

    Update MicronautComponentGenerator.java

--- MicroCli/src/main/java/io/hashimati/microcli/services/MicronautComponentGenerator.java
+++ MicroCli/src/main/java/io/hashimati/microcli/services/MicronautComponentGenerator.java
@@ -20,7 +20,7 @@ public class MicronautComponentGenerator {
 
     public String generateController(String classPackage, String className,String path,String lang){
         HashMap<String, String> map = new HashMap<>();
-        map.put("package", classPackage);
+        map.put("pack", classPackage);
         map.put("className", className);
         map.put("map", path);
         return generate(TemplatesService.COMP_CONTROLLER, map, lang);
@@ -28,21 +28,21 @@ public class MicronautComponentGenerator {
 
     public String generateSingleton(String classPackage, String className,String lang){
         HashMap<String, String> map = new HashMap<>();
-        map.put("package", classPackage);
+        map.put("pack", classPackage);
         map.put("className", className);
         return generate(TemplatesService.COMP_Singleton, map, lang);
     }
 //
 //    public String generateRepository(String classPackage, String className, String lang){
 //        HashMap<String, String> map = new HashMap<>();
-//        map.put("package", classPackage);
+//        map.put("pack", classPackage);
 //        map.put("className", className);
 //        return generate(TemplatesService.COMP_REPOSITORY, map, lang);
 //    }
 
     public String generateClient(String classPackage, String className,String serviceId, String lang){
         HashMap<String, String> map = new HashMap<>();
-        map.put("package", classPackage);
+        map.put("pack", classPackage);
         map.put("className", className);
         map.put("service-id", serviceId);
         return generate(TemplatesService.COMP_CLIENT, map, lang);
@@ -52,7 +52,7 @@ public class MicronautComponentGenerator {
     public String generateJob(String classPackage, String className,String lang){
 
         HashMap<String, String> map = new HashMap<>();
-        map.put("package", classPackage);
+        map.put("pack", classPackage);
         map.put("className", className);
         return generate(TemplatesService.COMP_JOB, map, lang);
 
@@ -60,7 +60,7 @@ public class MicronautComponentGenerator {
 
     public String generateWebSocket(String classPackage, String className, String path,String lang){
         HashMap<String, String> map = new HashMap<>();
-        map.put("package", classPackage);
+        map.put("pack", classPackage);
         map.put("className", className);
         map.put("path", path);
         return generate(TemplatesService.COMP_WEBSOCKET, map, lang);
@@ -69,7 +69,7 @@ public class MicronautComponentGenerator {
 
     public String generateWebsocketClient(String classPackage, String className, String path,String lang){
         HashMap<String, String> map = new HashMap<>();
-        map.put("package", classPackage);
+        map.put("pack", classPackage);
         map.put("className", className);
         map.put("path", path);
         return generate(TemplatesService.COMP_WEBSOCKET_CLIENT, map, lang);
@@ -77,7 +77,7 @@ public class MicronautComponentGenerator {
 
     public String generateKafkaClient(String classPackage, String topic, String message, String messagePackage,String className,String lang){
         HashMap<String, String> map = new HashMap<>();
-        map.put("package", classPackage);
+        map.put("pack", classPackage);
         map.put("className", className);
         map.put("topic", topic);
         map.put("Message", message);
@@ -89,7 +89,7 @@ public class MicronautComponentGenerator {
 
     public String generateKafkaConsumer(String classPackage, String groupId, String topic, String message, String messagePackage, String className,String lang){
         HashMap<String, String> map = new HashMap<>();
-        map.put("package", classPackage);
+        map.put("pack", classPackage);
         map.put("className", className);
         map.put("groupId", groupId);
         map.put("topic", topic);
@@ -102,7 +102,7 @@ public class MicronautComponentGenerator {
 
     public String generateRabbitMQClient(String classPackage, String className,String queueName,String message,String messagePackage,  String lang){
         HashMap<String, String> map = new HashMap<>();
-        map.put("package", classPackage);
+        map.put("pack", classPackage);
         map.put("className", className);
         map.put("queueName", queueName);
         map.put("Message", message);
@@ -114,7 +114,7 @@ public class MicronautComponentGenerator {
 
     public String generateRabbitMQConsumer(String classPackage, String className,String queueName, String message, String lang){
         HashMap<String, String> map = new HashMap<>();
-        map.put("package", classPackage);
+        map.put("pack", classPackage);
         map.put("className", className);
         map.put("queueName", queueName);
         map.put("Message", message);
@@ -126,22 +126,22 @@ public class MicronautComponentGenerator {
 
     public String generate(String key,HashMap<String, String> map,  String lang)
     {
-        String template = "";
+        String templatePath = "";
         switch (lang.toLowerCase())
         {
             case JAVA_LANG:
-                template =  templatesService.getJavaTemplates().get(key);
+                templatePath =  templatesService.getJavaTemplates().get(key);
                 break;
             case KOTLIN_LANG:
-                template =  templatesService.getKotlinTemplates().get(key);
+                templatePath =  templatesService.getKotlinTemplates().get(key);
                 break;
             case GROOVY_LANG:
-                template =  templatesService.getGroovyTemplates().get(key);
+                templatePath =  templatesService.getGroovyTemplates().get(key);
                 break;
             default:
-                template =  templatesService.getJavaTemplates().get(key);
+                templatePath =  templatesService.getJavaTemplates().get(key);
                 break;
         }
-        return GeneratorUtils.generateFromTemplate(template, map);
+        return GeneratorUtils.generateFromTemplate(templatesService.loadTemplateContent(templatePath), map);
     }
 }
