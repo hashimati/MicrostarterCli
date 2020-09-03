0c296dd5e51e4d35de3baea443245b0188cd448c
Ahmed Al Hashmi <hashimati.ahmed@gmail.com>
2020-09-04 01:14:04 +0300

    Create CreateRabbitMQListenerCommand.java

---
+++ MicroCli/src/main/java/io/hashimati/microcli/commands/CreateRabbitMQListenerCommand.java
@@ -0,0 +1,13 @@
+package io.hashimati.microcli.commands;
+
+import picocli.CommandLine;
+
+import java.util.concurrent.Callable;
+
+@CommandLine.Command(name= "create-rabbitmq-listener", aliases = {"rabbitmqClient", "rabbitmq-Listener"}, description = "To create rabbitmq listener component")
+public class CreateRabbitMQListenerCommand implements Callable<Integer> {
+    @Override
+    public Integer call() throws Exception {
+        return 0;
+    }
+}
