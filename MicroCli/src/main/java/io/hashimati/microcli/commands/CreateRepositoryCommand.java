fa88e8107ab3c218c4386e792785ae8ca6d69653
Ahmed Al Hashmi <hashimati.ahmed@gmail.com>
2020-09-04 00:53:47 +0300

    Update CreateRepositoryCommand.java
    Adding File

--- MicroCli/src/main/java/io/hashimati/microcli/commands/CreateRepositoryCommand.java
+++ MicroCli/src/main/java/io/hashimati/microcli/commands/CreateRepositoryCommand.java
@@ -2,6 +2,7 @@ package io.hashimati.microcli.commands;
 
 import java.util.concurrent.Callable;
 
+@Deprecated
 public class CreateRepositoryCommand implements Callable<Integer> {
     @Override
     public Integer call() throws Exception {
