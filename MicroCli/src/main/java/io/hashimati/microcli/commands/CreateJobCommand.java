e029f0828417da276c90b0ce86a121140a73f800
Ahmed Al Hashmi <hashimati.ahmed@gmail.com>
2020-09-04 00:52:33 +0300

    Update CreateJobCommand.java
    Create Job Command implementation

--- MicroCli/src/main/java/io/hashimati/microcli/commands/CreateJobCommand.java
+++ MicroCli/src/main/java/io/hashimati/microcli/commands/CreateJobCommand.java
@@ -1,10 +1,51 @@
 package io.hashimati.microcli.commands;
 
+import io.hashimati.microcli.constants.ProjectConstants;
+import io.hashimati.microcli.domains.ConfigurationInfo;
+import io.hashimati.microcli.services.MicronautComponentGenerator;
+import io.hashimati.microcli.utils.GeneratorUtils;
+import io.hashimati.microcli.utils.PromptGui;
+import org.fusesource.jansi.AnsiConsole;
+import picocli.CommandLine;
+
+import javax.inject.Inject;
+import java.io.File;
+import java.util.HashMap;
 import java.util.concurrent.Callable;
 
+import static io.hashimati.microcli.utils.GeneratorUtils.createFile;
+import static io.hashimati.microcli.utils.PromptGui.printlnSuccess;
+import static io.hashimati.microcli.utils.PromptGui.setToDefault;
+
+
+@CommandLine.Command(name= "create-job", aliases = {"job", "async", "cron", "cron-job"}, description = "To create cron job component")
 public class CreateJobCommand implements Callable<Integer> {
+    @Inject
+    private MicronautComponentGenerator micronautComponentGenerator;
     @Override
     public Integer call() throws Exception {
-        return null;
+
+        AnsiConsole.systemInstall();
+        ConfigurationInfo configurationInfo = ConfigurationInfo.fromFile(new File(ConfigurationInfo.getConfigurationFileName()) );
+        String packageName = PromptGui.inputText("pack", "Enter the job's package: ", configurationInfo.getProjectInfo().getDefaultPackage()).getInput();
+
+        String className = PromptGui.inputText("className", "Enter job name: ", "MyJob").getInput();
+        String lang = configurationInfo.getProjectInfo().getSourceLanguage();
+        String content = micronautComponentGenerator.generateJob(packageName, className,lang);
+
+
+
+        String extension = GeneratorUtils.getSourceFileExtension(lang);
+        String controllerPath = GeneratorUtils.generateFromTemplate(ProjectConstants.PathsTemplate.GENERAL_PATH, new HashMap<String, String>(){{
+            put("lang", configurationInfo.getProjectInfo().getSourceLanguage());
+            put("defaultPackage", GeneratorUtils.packageToPath(packageName));
+        }});
+
+        createFile(System.getProperty("user.dir")+controllerPath+ "/"+className+extension, content);
+
+        printlnSuccess(className + " is create successfully!");
+        setToDefault();
+
+        return 0;
     }
 }
