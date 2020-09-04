ee68dcc83d648be4fede4e8ae96eeebd9dec173c
Ahmed Al Hashmi <hashimati.ahmed@gmail.com>
2020-09-04 16:04:00 +0300

    Update CreateKafkaListenerCommand.java

--- MicroCli/src/main/java/io/hashimati/microcli/commands/CreateKafkaListenerCommand.java
+++ MicroCli/src/main/java/io/hashimati/microcli/commands/CreateKafkaListenerCommand.java
@@ -9,6 +9,7 @@ import java.util.concurrent.Callable;
 public class CreateKafkaListenerCommand implements Callable<Integer> {
     @Override
     public Integer call() throws Exception {
+
         return 0;
     }
 }
