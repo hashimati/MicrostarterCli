5357133823cabd34c6595b816515123533e59728
Ahmed Al Hashmi <hashimati.ahmed@gmail.com>
2020-09-05 00:36:48 +0300

    Update CreateKafkaListenerCommand.java
    reverting

--- MicroCli/src/main/java/io/hashimati/microcli/commands/CreateKafkaListenerCommand.java
+++ MicroCli/src/main/java/io/hashimati/microcli/commands/CreateKafkaListenerCommand.java
@@ -1,62 +1,15 @@
 package io.hashimati.microcli.commands;
 
 
-import io.hashimati.microcli.constants.ProjectConstants;
-import io.hashimati.microcli.domains.ConfigurationInfo;
-import io.hashimati.microcli.services.MicronautComponentGenerator;
-import io.hashimati.microcli.utils.GeneratorUtils;
-import io.hashimati.microcli.utils.PromptGui;
-import org.fusesource.jansi.AnsiConsole;
+import picocli.CommandLine;
 import picocli.CommandLine.Command;
 
-import javax.inject.Inject;
-import java.io.File;
-import java.util.HashMap;
 import java.util.concurrent.Callable;
-
-import static io.hashimati.microcli.utils.GeneratorUtils.createFile;
-import static io.hashimati.microcli.utils.PromptGui.printlnSuccess;
-import static io.hashimati.microcli.utils.PromptGui.setToDefault;
 
 @Command(name="create-kafka-listener", aliases = {"kafka-listener", "kafkaListener"}, description = "Creating Kafak Listener componenet")
 public class CreateKafkaListenerCommand implements Callable<Integer> {
-
-    @Inject
-    private MicronautComponentGenerator micronautComponentGenerator;
-
     @Override
     public Integer call() throws Exception {
-
-        AnsiConsole.systemInstall();
-        File configurationFile =new File(ConfigurationInfo.getConfigurationFileName());
-        ConfigurationInfo  configurationInfo;
-        if(!configurationFile.exists()){
-            configurationInfo =  new ConfigureCommand().call();
-        }
-        else {
-            configurationInfo = ConfigurationInfo.fromFile(configurationFile);
-        }
-        String packageName = PromptGui.inputText("pack", "Enter the controller's package: ", configurationInfo.getProjectInfo().getDefaultPackage()).getInput();
-
-        String className = PromptGui.inputText("className", "Enter controller name: ", "MyController").getInput();
-
-        String path = PromptGui.inputText("map", "Enter the controller's path: ", "/"+className).getInput();
-        String lang = configurationInfo.getProjectInfo().getSourceLanguage();
-        String content = micronautComponentGenerator.generateController(packageName, className, path,lang);
-
-
-
-        String extension = GeneratorUtils.getSourceFileExtension(lang);
-        String controllerPath = GeneratorUtils.generateFromTemplate(ProjectConstants.PathsTemplate.GENERAL_PATH, new HashMap<String, String>(){{
-            put("lang", configurationInfo.getProjectInfo().getSourceLanguage());
-            put("defaultPackage", GeneratorUtils.packageToPath(packageName));
-        }});
-
-        createFile(System.getProperty("user.dir")+controllerPath+ "/"+className+extension, content);
-
-        printlnSuccess(className + " is create successfully!");
-        setToDefault();
-
         return 0;
     }
 }
