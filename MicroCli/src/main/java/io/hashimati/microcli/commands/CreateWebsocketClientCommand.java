83fff2f390a1e7ece243a79c12932166408a5385
Ahmed Al Hashmi <hashimati.ahmed@gmail.com>
2020-09-04 12:02:40 +0300

    Update CreateWebsocketClientCommand.java
    implement generate websocket client command

--- MicroCli/src/main/java/io/hashimati/microcli/commands/CreateWebsocketClientCommand.java
+++ MicroCli/src/main/java/io/hashimati/microcli/commands/CreateWebsocketClientCommand.java
@@ -1,13 +1,53 @@
 package io.hashimati.microcli.commands;
 
+import io.hashimati.microcli.constants.ProjectConstants;
+import io.hashimati.microcli.domains.ConfigurationInfo;
+import io.hashimati.microcli.services.MicronautComponentGenerator;
+import io.hashimati.microcli.utils.GeneratorUtils;
+import io.hashimati.microcli.utils.PromptGui;
+import org.fusesource.jansi.AnsiConsole;
 import picocli.CommandLine.Command;
 
+import javax.inject.Inject;
+import java.io.File;
+import java.util.HashMap;
 import java.util.concurrent.Callable;
+
+import static io.hashimati.microcli.utils.GeneratorUtils.createFile;
+import static io.hashimati.microcli.utils.PromptGui.printlnSuccess;
+import static io.hashimati.microcli.utils.PromptGui.setToDefault;
 
 @Command(name= "create-websocket-client", aliases = {"websocketClient", "websocket-client"}, description = "To create websocket client component")
 public class CreateWebsocketClientCommand implements Callable<Integer> {
+
+    @Inject
+    private MicronautComponentGenerator micronautComponentGenerator;
     @Override
     public Integer call() throws Exception {
+
+        AnsiConsole.systemInstall();
+        ConfigurationInfo configurationInfo = ConfigurationInfo.fromFile(new File(ConfigurationInfo.getConfigurationFileName()) );
+        String packageName = PromptGui.inputText("pack", "Enter the websocket client's package: ", configurationInfo.getProjectInfo().getDefaultPackage()).getInput();
+
+        String className = PromptGui.inputText("className", "Enter websocket client name: ", "MyController").getInput();
+
+        String path = PromptGui.inputText("map", "Enter the websocket path: ", "/"+className).getInput();
+        String lang = configurationInfo.getProjectInfo().getSourceLanguage();
+        String content = micronautComponentGenerator.generateWebsocketClient(packageName, className, path,lang);
+
+
+
+        String extension = GeneratorUtils.getSourceFileExtension(lang);
+        String controllerPath = GeneratorUtils.generateFromTemplate(ProjectConstants.PathsTemplate.GENERAL_PATH, new HashMap<String, String>(){{
+            put("lang", configurationInfo.getProjectInfo().getSourceLanguage());
+            put("defaultPackage", GeneratorUtils.packageToPath(packageName));
+        }});
+
+        createFile(System.getProperty("user.dir")+controllerPath+ "/"+className+extension, content);
+
+        printlnSuccess(className + " is create successfully!");
+        setToDefault();
+
         return 0;
     }
 }
