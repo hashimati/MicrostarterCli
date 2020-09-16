9e8667beafdea3f811c5169985a5990348fb0c33
Ahmed Al Hashmi <hashimati.ahmed@gmail.com>
2020-09-16 23:42:27 +0300

    Update CreateRabbitMQListenerCommand.java
    Create RabbitMQ Listener Command

--- MicroCli/src/main/java/io/hashimati/microcli/commands/CreateRabbitMQListenerCommand.java
+++ MicroCli/src/main/java/io/hashimati/microcli/commands/CreateRabbitMQListenerCommand.java
@@ -1,13 +1,76 @@
 package io.hashimati.microcli.commands;
 
+import io.hashimati.microcli.constants.ProjectConstants;
+import io.hashimati.microcli.domains.ConfigurationInfo;
+import io.hashimati.microcli.domains.Entity;
+import io.hashimati.microcli.services.MicronautComponentGenerator;
+import io.hashimati.microcli.utils.GeneratorUtils;
+import io.hashimati.microcli.utils.PromptGui;
+import org.fusesource.jansi.AnsiConsole;
 import picocli.CommandLine;
 
+import javax.inject.Inject;
+import java.io.File;
+import java.util.HashMap;
 import java.util.concurrent.Callable;
+import java.util.stream.Collectors;
+
+import static io.hashimati.microcli.utils.GeneratorUtils.createFile;
+import static io.hashimati.microcli.utils.PromptGui.printlnSuccess;
+import static io.hashimati.microcli.utils.PromptGui.setToDefault;
 
 @CommandLine.Command(name= "create-rabbitmq-listener", aliases = {"rabbitmqListener", "rabbitmq-Listener"}, description = "To create rabbitmq listener component")
 public class CreateRabbitMQListenerCommand implements Callable<Integer> {
+
+    @CommandLine.Option(names = {"-e", "--entity"})
+    String entityName;
+
+    @Inject
+    private MicronautComponentGenerator micronautComponentGenerator;
+
     @Override
     public Integer call() throws Exception {
-        return 0;
+
+        AnsiConsole.systemInstall();
+        File configurationFile =new File(ConfigurationInfo.getConfigurationFileName());
+        ConfigurationInfo  configurationInfo;
+        if(!configurationFile.exists()){
+            configurationInfo =  new ConfigureCommand().call();
+        }
+        else {
+            configurationInfo = ConfigurationInfo.fromFile(configurationFile);
+        }
+
+        String packageName = PromptGui.inputText("pack", "Enter the class's package: ", configurationInfo.getProjectInfo().getDefaultPackage()).getInput();
+
+
+        String className = PromptGui.inputText("className", "Enter the class name: ", "RabbitMQListener").getInput();
+
+        String queueName = PromptGui.inputText("queue", "Enter the queue name", className).getInput();
+        if(entityName == null)
+        {
+            entityName = PromptGui.createListPrompt("entity", "Select Message Type: ",
+                    configurationInfo.getEntities().stream().map(x->x.getName()).collect(Collectors.toList())
+                            .toArray(new String[configurationInfo.getEntities().size()])).getSelectedId();
+        }
+
+
+        Entity entity = configurationInfo.getEntities().stream().filter(x->x.getName().equals(this.entityName)).findFirst().get();
+
+        String lang = configurationInfo.getProjectInfo().getSourceLanguage();
+        String content = micronautComponentGenerator.generateRabbitMQConsumer(packageName, className,queueName, entity,lang);
+
+
+        String extension = GeneratorUtils.getSourceFileExtension(lang);
+        String controllerPath = GeneratorUtils.generateFromTemplate(ProjectConstants.PathsTemplate.GENERAL_PATH, new HashMap<String, String>(){{
+            put("lang", configurationInfo.getProjectInfo().getSourceLanguage());
+            put("defaultPackage", GeneratorUtils.packageToPath(packageName));
+        }});
+
+        createFile(System.getProperty("user.dir")+controllerPath+ "/"+className+extension, content);
+
+        printlnSuccess(className + " is create successfully!");
+        setToDefault();
+        return null;
     }
 }
