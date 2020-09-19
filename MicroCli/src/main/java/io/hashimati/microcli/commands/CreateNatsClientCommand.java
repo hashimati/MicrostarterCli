744a084c51736969d57f73b90aa708079e6baf5c
Ahmed Al Hashmi <hashimati.ahmed@gmail.com>
2020-09-19 14:18:33 +0300

    Create CreateNatsClientCommand.java

---
+++ MicroCli/src/main/java/io/hashimati/microcli/commands/CreateNatsClientCommand.java
@@ -0,0 +1,73 @@
+package io.hashimati.microcli.commands;
+
+import io.hashimati.microcli.constants.ProjectConstants;
+import io.hashimati.microcli.domains.ConfigurationInfo;
+import io.hashimati.microcli.domains.Entity;
+import io.hashimati.microcli.services.MicronautComponentGenerator;
+import io.hashimati.microcli.utils.GeneratorUtils;
+import io.hashimati.microcli.utils.PromptGui;
+import org.fusesource.jansi.AnsiConsole;
+import picocli.CommandLine;
+
+import javax.inject.Inject;
+import java.io.File;
+import java.util.HashMap;
+import java.util.concurrent.Callable;
+import java.util.stream.Collectors;
+
+import static io.hashimati.microcli.utils.GeneratorUtils.createFile;
+import static io.hashimati.microcli.utils.PromptGui.printlnSuccess;
+import static io.hashimati.microcli.utils.PromptGui.setToDefault;
+
+@CommandLine.Command(name= "create-nats-client", aliases = {"nats-client", "natsClient", "NatsClient"}, description = "To create Nats Client.")
+public class CreateNatsClientCommand implements Callable<Integer> {
+
+    @CommandLine.Option(names = {"-e", "--entity"})
+    String entityName;
+
+    @Inject
+    private MicronautComponentGenerator micronautComponentGenerator;
+
+    @Override
+    public Integer call() throws Exception {
+
+        AnsiConsole.systemInstall();
+        File configurationFile =new File(ConfigurationInfo.getConfigurationFileName());
+        ConfigurationInfo  configurationInfo;
+        if(!configurationFile.exists()){
+            configurationInfo =  new ConfigureCommand().call();
+        }
+        else {
+            configurationInfo = ConfigurationInfo.fromFile(configurationFile);
+        }
+
+        String packageName = PromptGui.inputText("pack", "Enter the class's package: ", configurationInfo.getProjectInfo().getDefaultPackage()).getInput();
+        String className = PromptGui.inputText("className", "Enter the class name: ", "NatsListener").getInput();
+        String topic = PromptGui.inputText("topic", "Enter the subject name", className).getInput();
+        if(entityName == null)
+        {
+            entityName = PromptGui.createListPrompt("entity", "Select Message Type: ",
+                    configurationInfo.getEntities().stream().map(x->x.getName()).collect(Collectors.toList())
+                            .toArray(new String[configurationInfo.getEntities().size()])).getSelectedId();
+        }
+
+
+        Entity entity = configurationInfo.getEntities().stream().filter(x->x.getName().equals(this.entityName)).findFirst().get();
+
+        String lang = configurationInfo.getProjectInfo().getSourceLanguage();
+        String content = micronautComponentGenerator.generateNatsClient(packageName, className, topic, entity,lang);
+
+
+        String extension = GeneratorUtils.getSourceFileExtension(lang);
+        String controllerPath = GeneratorUtils.generateFromTemplate(ProjectConstants.PathsTemplate.GENERAL_PATH, new HashMap<String, String>(){{
+            put("lang", configurationInfo.getProjectInfo().getSourceLanguage());
+            put("defaultPackage", GeneratorUtils.packageToPath(packageName));
+        }});
+
+        createFile(System.getProperty("user.dir")+controllerPath+ "/"+className+extension, content);
+
+        printlnSuccess(className + " is create successfully!");
+        setToDefault();
+        return null;
+    }
+}
