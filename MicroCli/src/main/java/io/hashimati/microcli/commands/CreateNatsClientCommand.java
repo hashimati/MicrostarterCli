c2afec672e75f0d6fd6ee1b9de938ed351c0c059
Ahmed Al Hashmi <hashimati.ahmed@gmail.com>
2020-09-27 02:25:52 +0300

    add Nats Dependencies
    add Nats Dependencies

--- MicroCli/src/main/java/io/hashimati/microcli/commands/CreateNatsClientCommand.java
+++ MicroCli/src/main/java/io/hashimati/microcli/commands/CreateNatsClientCommand.java
@@ -1,10 +1,14 @@
 package io.hashimati.microcli.commands;
 
+import io.hashimati.microcli.config.FeaturesFactory;
 import io.hashimati.microcli.constants.ProjectConstants;
 import io.hashimati.microcli.domains.ConfigurationInfo;
 import io.hashimati.microcli.domains.Entity;
+import io.hashimati.microcli.domains.ProjectInfo;
 import io.hashimati.microcli.services.MicronautComponentGenerator;
+import io.hashimati.microcli.services.TemplatesService;
 import io.hashimati.microcli.utils.GeneratorUtils;
+import io.hashimati.microcli.utils.MicronautProjectValidator;
 import io.hashimati.microcli.utils.PromptGui;
 import org.fusesource.jansi.AnsiConsole;
 import picocli.CommandLine;
@@ -39,6 +43,28 @@ public class CreateNatsClientCommand implements Callable<Integer> {
         }
         else {
             configurationInfo = ConfigurationInfo.fromFile(configurationFile);
+        }
+        ProjectInfo projectInfo = configurationInfo.getProjectInfo();
+        //add NATS Dependencies
+        if(!configurationInfo
+                .getProjectInfo().getFeatures().contains("nats")){
+
+            TemplatesService templatesService = new TemplatesService();
+            projectInfo.getFeatures().add("nats");
+            MicronautProjectValidator.addDependency(FeaturesFactory.features().get("nats"));
+
+           projectInfo.dumpToFile();
+
+
+            //AddingYaml
+            templatesService.loadTemplates(null);
+            String messagingProperties = templatesService.loadTemplateContent
+                    (templatesService.getProperties().get("nats"));
+            MicronautProjectValidator.appendToProperties(messagingProperties);
+
+            configurationInfo.writeToFile(); 
+            // End adding Yaml
+
         }
 
         String packageName = PromptGui.inputText("pack", "Enter the class's package: ", configurationInfo.getProjectInfo().getDefaultPackage()).getInput();
