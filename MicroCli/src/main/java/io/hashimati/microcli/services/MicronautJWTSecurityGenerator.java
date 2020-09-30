43a8ea9b901fee5f9627c3774eb17f667859cda8
Ahmed Al Hashmi <hashimati.ahmed@gmail.com>
2020-09-30 16:49:14 +0300

    Update MicronautJWTSecurityGenerator.java

--- MicroCli/src/main/java/io/hashimati/microcli/services/MicronautJWTSecurityGenerator.java
+++ MicroCli/src/main/java/io/hashimati/microcli/services/MicronautJWTSecurityGenerator.java
@@ -1,6 +1,6 @@
 package io.hashimati.microcli.services;
 
-import io.hashimati.microcli.constants.ProjectConstants;
+import io.hashimati.microcli.domains.ConfigurationInfo;
 import io.hashimati.microcli.utils.GeneratorUtils;
 
 import javax.inject.Inject;
@@ -16,78 +16,90 @@ public class MicronautJWTSecurityGenerator {
     @Inject
     private TemplatesService templatesService;
 
-    public String generateUserEntity(String language)
+    private ConfigurationInfo configurationInfo;
+
+    public String generateUserEntity(String defaultPackage, String language)
     {
         String key =  TemplatesService.SECURITY_USER;
         HashMap<String, String> map = new HashMap<>();
+        map.put("securityPackage", new StringBuilder().append(defaultPackage).append(".security").toString());
+
 
         return generate(key, map, language );
     }
-    public String generateUserRepository(String language, String database)
+    public String generateUserRepository(String defaultPackage,String language, String database)
     {
 
         String key = database.equalsIgnoreCase("mongodb")? TemplatesService.SECURITY_USER_MONGO_REPOSITORY: TemplatesService.SECURITY_USER_REPOSITORY;
 
         HashMap<String, String> map = new HashMap<>();
+        map.put("securityPackage", new StringBuilder().append(defaultPackage).append(".security").toString());
 
         return generate(key, map, language );
     }
-    public String generateUserService(String language, String database)
+    public String generateUserService(String defaultPackage,String language, String database)
     {
 
         String key = database.equalsIgnoreCase("mongodb")? TemplatesService.SECURITY_USER_MONGO_REPOSITORY: TemplatesService.SECURITY_USER_REPOSITORY;
 
         HashMap<String, String> map = new HashMap<>();
+        map.put("securityPackage", new StringBuilder().append(defaultPackage).append(".security").toString());
 
         return generate(key, map, language );
     }
-    public String generateUserController(String language, String database)
+    public String generateUserController(String defaultPackage,String language, String database)
     {
 
         String key = database.equalsIgnoreCase("mongodb")? TemplatesService.SECURITY_USER_MONGO_REPOSITORY: TemplatesService.SECURITY_USER_REPOSITORY;
 
         HashMap<String, String> map = new HashMap<>();
+        map.put("securityPackage", new StringBuilder().append(defaultPackage).append(".security").toString());
 
         return generate(key, map, language );
     }
-    public String generatePasswrodEncoder(String language){
+    public String generatePasswrodEncoder(String defaultPackage,String language){
         String key =  TemplatesService.SECURITY_PASSWORD_ENCODER;
 
         HashMap<String, String> map = new HashMap<>();
+        map.put("securityPackage", new StringBuilder().append(defaultPackage).append(".security").toString());
 
         return generate(key, map, language );
 
     }
-    public String generateAuthenticationProvider(String language)
+    public String generateAuthenticationProvider(String defaultPackage,String language)
     {
         String key =  TemplatesService.SECURITY_AUTHENTICATION_PROVIDER;
 
-        HashMap<String, String> map = new HashMap<>();
 
+        HashMap<String, String> map = new HashMap<>();
+        map.put("securityPackage", new StringBuilder().append(defaultPackage).append(".security").toString());
         return generate(key, map, language );
     }
-    public String generateLoginEvent(String language){
+    public String generateLoginEvent(String defaultPackage,String language){
         String key =  TemplatesService.SECURITY_LOGIN_EVENT;
 
         HashMap<String, String> map = new HashMap<>();
+        map.put("securityPackage", new StringBuilder().append(defaultPackage).append(".security").toString());
 
         return generate(key, map, language );
     }
-    public String generateLoginStatus(String language){
+    public String generateLoginStatus(String defaultPackage,String language){
         String key =  TemplatesService.SECURITY_LOGIN_STATUS;
 
         HashMap<String, String> map = new HashMap<>();
+        map.put("securityPackage", new StringBuilder().append(defaultPackage).append(".security").toString());
 
         return generate(key, map, language );
     }
-    public String generateRoles( String language, String... roles){
+    public String generateRoles( String defaultPackage,String language, String... roles){
 
-        return generateRoles(language ,Arrays.asList(roles));
+        return generateRoles(defaultPackage,language ,Arrays.asList(roles));
     }
-    public String generateRoles(String language,List<String> roles){
+    public String generateRoles(String defaultPackage,String language,List<String> roles){
         String key =  TemplatesService.SECURITY_ROLES;
 
         HashMap<String, String> map = new HashMap<>();
+        map.put("securityPackage", new StringBuilder().append(defaultPackage).append(".security").toString());
 
         return generate(key, map, language );
     }
