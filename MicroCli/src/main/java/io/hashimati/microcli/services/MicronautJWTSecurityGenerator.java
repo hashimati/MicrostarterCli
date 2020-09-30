cb73f9d87c28ceb7be99b22628814cc7272f6a72
Ahmed Al Hashmi <hashimati.ahmed@gmail.com>
2020-09-30 16:22:39 +0300

    Ceaning Code
    Ceaning Code

--- MicroCli/src/main/java/io/hashimati/microcli/services/MicronautJWTSecurityGenerator.java
+++ MicroCli/src/main/java/io/hashimati/microcli/services/MicronautJWTSecurityGenerator.java
@@ -1,14 +1,15 @@
 package io.hashimati.microcli.services;
 
 import io.hashimati.microcli.constants.ProjectConstants;
+import io.hashimati.microcli.utils.GeneratorUtils;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
-import static io.hashimati.microcli.constants.ProjectConstants.LanguagesConstants.GROOVY_LANG;
-import static io.hashimati.microcli.constants.ProjectConstants.LanguagesConstants.KOTLIN_LANG;
+import static io.hashimati.microcli.constants.ProjectConstants.LanguagesConstants.*;
 
 @Singleton
 public class MicronautJWTSecurityGenerator {
@@ -17,128 +18,100 @@ public class MicronautJWTSecurityGenerator {
 
     public String generateUserEntity(String language)
     {
-        String templatePath;
-        if(language.equalsIgnoreCase(GROOVY_LANG)) {
-            templatePath = templatesService.getGroovyTemplates().get(TemplatesService.SECURITY_USER);
-        } else if(language.equalsIgnoreCase(KOTLIN_LANG)) {
-            templatePath = templatesService.getKotlinTemplates().get(TemplatesService.SECURITY_USER);
-        }
-        else
-            templatePath = templatesService.getJavaTemplates().get(TemplatesService.SECURITY_USER);
+        String key =  TemplatesService.SECURITY_USER;
+        HashMap<String, String> map = new HashMap<>();
 
-        return "";
+        return generate(key, map, language );
     }
     public String generateUserRepository(String language, String database)
     {
-        String templatePath;
-        String key = database.equalsIgnoreCase("mongodb")? TemplatesService.SECURITY_USER_MONGO_REPOSITORY: TemplatesService.SECURITY_USER_REPOSITORY;
-        if(language.equalsIgnoreCase(GROOVY_LANG)) {
-            templatePath = templatesService.getGroovyTemplates().get(key);
-        } else if(language.equalsIgnoreCase(KOTLIN_LANG)) {
-            templatePath = templatesService.getKotlinTemplates().get(key);
-        }
-        else
-            templatePath = templatesService.getJavaTemplates().get(key);
 
-        return "";
+        String key = database.equalsIgnoreCase("mongodb")? TemplatesService.SECURITY_USER_MONGO_REPOSITORY: TemplatesService.SECURITY_USER_REPOSITORY;
+
+        HashMap<String, String> map = new HashMap<>();
+
+        return generate(key, map, language );
     }
     public String generateUserService(String language, String database)
     {
-        String templatePath;
-        String key = database.equalsIgnoreCase("mongodb")? TemplatesService.SECURITY_USER_MONGO_SERVICE: TemplatesService.SECURITY_USER_SERVICE;
 
-        if(language.equalsIgnoreCase(GROOVY_LANG)) {
-            templatePath = templatesService.getGroovyTemplates().get(key);
-        } else if(language.equalsIgnoreCase(KOTLIN_LANG)) {
-            templatePath = templatesService.getKotlinTemplates().get(key);
-        }
-        else
-            templatePath = templatesService.getJavaTemplates().get(key);
+        String key = database.equalsIgnoreCase("mongodb")? TemplatesService.SECURITY_USER_MONGO_REPOSITORY: TemplatesService.SECURITY_USER_REPOSITORY;
 
-        return "";
+        HashMap<String, String> map = new HashMap<>();
+
+        return generate(key, map, language );
     }
     public String generateUserController(String language, String database)
     {
-        String templatePath;
-        String key = database.equalsIgnoreCase("mongodb")? TemplatesService.SECURITY_USER_MONGO_CONTROLLER: TemplatesService.SECURITY_USER_CONTROLLER;
 
-        if(language.equalsIgnoreCase(GROOVY_LANG)) {
-            templatePath = templatesService.getGroovyTemplates().get(key);
-        } else if(language.equalsIgnoreCase(KOTLIN_LANG)) {
-            templatePath = templatesService.getKotlinTemplates().get(key);
-        }
-        else
-            templatePath = templatesService.getJavaTemplates().get(key);
+        String key = database.equalsIgnoreCase("mongodb")? TemplatesService.SECURITY_USER_MONGO_REPOSITORY: TemplatesService.SECURITY_USER_REPOSITORY;
 
+        HashMap<String, String> map = new HashMap<>();
 
-        return "";
+        return generate(key, map, language );
     }
     public String generatePasswrodEncoder(String language){
-        String templatePath;
-        if(language.equalsIgnoreCase(GROOVY_LANG)) {
-            templatePath = templatesService.getGroovyTemplates().get(TemplatesService.SECURITY_PASSWORD_ENCODER);
-        } else if(language.equalsIgnoreCase(KOTLIN_LANG)) {
-            templatePath = templatesService.getKotlinTemplates().get(TemplatesService.SECURITY_PASSWORD_ENCODER);
-        }
-        else
-            templatePath = templatesService.getJavaTemplates().get(TemplatesService.SECURITY_PASSWORD_ENCODER);
+        String key =  TemplatesService.SECURITY_PASSWORD_ENCODER;
 
-        return "";
+        HashMap<String, String> map = new HashMap<>();
+
+        return generate(key, map, language );
 
     }
     public String generateAuthenticationProvider(String language)
     {
-        String templatePath;
-        if(language.equalsIgnoreCase(GROOVY_LANG)) {
-            templatePath = templatesService.getGroovyTemplates().get(TemplatesService.SECURITY_AUTHENTICATION_PROVIDER);
-        } else if(language.equalsIgnoreCase(KOTLIN_LANG)) {
-            templatePath = templatesService.getKotlinTemplates().get(TemplatesService.SECURITY_AUTHENTICATION_PROVIDER);
-        }
-        else
-            templatePath = templatesService.getJavaTemplates().get(TemplatesService.SECURITY_AUTHENTICATION_PROVIDER);
+        String key =  TemplatesService.SECURITY_AUTHENTICATION_PROVIDER;
 
-        return "";
+        HashMap<String, String> map = new HashMap<>();
+
+        return generate(key, map, language );
     }
     public String generateLoginEvent(String language){
-        String templatePath;
-        if(language.equalsIgnoreCase(GROOVY_LANG)) {
-            templatePath = templatesService.getGroovyTemplates().get(TemplatesService.SECURITY_LOGIN_EVENT);
-        } else if(language.equalsIgnoreCase(KOTLIN_LANG)) {
-            templatePath = templatesService.getKotlinTemplates().get(TemplatesService.SECURITY_LOGIN_EVENT);
-        }
-        else
-            templatePath = templatesService.getJavaTemplates().get(TemplatesService.SECURITY_LOGIN_EVENT);
+        String key =  TemplatesService.SECURITY_LOGIN_EVENT;
 
-        return "";
+        HashMap<String, String> map = new HashMap<>();
+
+        return generate(key, map, language );
     }
     public String generateLoginStatus(String language){
-        String templatePath;
-        if(language.equalsIgnoreCase(GROOVY_LANG)) {
-            templatePath = templatesService.getGroovyTemplates().get(TemplatesService.SECURITY_LOGIN_STATUS);
-        } else if(language.equalsIgnoreCase(KOTLIN_LANG)) {
-            templatePath = templatesService.getKotlinTemplates().get(TemplatesService.SECURITY_LOGIN_STATUS);
-        }
-        else
-            templatePath = templatesService.getJavaTemplates().get(TemplatesService.SECURITY_LOGIN_STATUS);
+        String key =  TemplatesService.SECURITY_LOGIN_STATUS;
 
-        return "";
+        HashMap<String, String> map = new HashMap<>();
+
+        return generate(key, map, language );
     }
     public String generateRoles( String language, String... roles){
 
         return generateRoles(language ,Arrays.asList(roles));
     }
     public String generateRoles(String language,List<String> roles){
-        String templatePath;
-        if(language.equalsIgnoreCase(GROOVY_LANG)) {
-            templatePath = templatesService.getGroovyTemplates().get(TemplatesService.SECURITY_ROLES);
-        } else if(language.equalsIgnoreCase(KOTLIN_LANG)) {
-            templatePath = templatesService.getKotlinTemplates().get(TemplatesService.SECURITY_ROLES);
+        String key =  TemplatesService.SECURITY_ROLES;
+
+        HashMap<String, String> map = new HashMap<>();
+
+        return generate(key, map, language );
+    }
+
+
+    public String generate(String key, HashMap<String, String> map, String lang)
+    {
+        String templatePath = "";
+        switch (lang.toLowerCase())
+        {
+            case JAVA_LANG:
+                templatePath =  templatesService.getJavaTemplates().get(key);
+                break;
+            case KOTLIN_LANG:
+                templatePath =  templatesService.getKotlinTemplates().get(key);
+                break;
+            case GROOVY_LANG:
+                templatePath =  templatesService.getGroovyTemplates().get(key);
+                break;
+            default:
+                templatePath =  templatesService.getJavaTemplates().get(key);
+                break;
         }
-        else
-            templatePath = templatesService.getJavaTemplates().get(TemplatesService.SECURITY_ROLES);
-
-
-        return "";
+        return GeneratorUtils.generateFromTemplate(templatesService.loadTemplateContent(templatePath), map);
     }
 
 }
