c4450ed069b0c932458af74c54f3d84997c0f326
Ahmed Al Hashmi <hashimati.ahmed@gmail.com>
2020-10-02 19:31:22 +0300

    Create PasswordEncoder.java

---
+++ Code Examples/JWTwithJDBC/src/main/java/com/example/security/PasswordEncoder.java
@@ -0,0 +1,17 @@
+package com.example.security;
+
+import org.jasypt.util.password.StrongPasswordEncryptor;
+
+import io.micronaut.context.annotation.Factory;
+import io.micronaut.context.annotation.Prototype;
+
+@Factory
+public class PasswordEncoder {
+
+    @Prototype
+    public StrongPasswordEncryptor strongPasswordEncryptor(){
+        return new StrongPasswordEncryptor();
+    }
+
+}
+
