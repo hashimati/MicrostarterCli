156cbce3fc633614364ecdda9a1df6a08bd626a7
Ahmed Al Hashmi <hashimati.ahmed@gmail.com>
2020-10-02 19:31:31 +0300

    Create Application.java

---
+++ Code Examples/JWTwithJDBC/src/main/java/com/example/Application.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import io.micronaut.runtime.Micronaut;
+import io.swagger.v3.oas.annotations.*;
+import io.swagger.v3.oas.annotations.info.*;
+
+@OpenAPIDefinition(
+    info = @Info(
+            title = "demo",
+            version = "0.0"
+    )
+)
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
