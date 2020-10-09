79a3fdf03225cdd53d598124f9ac6b3f9b5ba8df
Ahmed Al Hashmi <hashimati.ahmed@gmail.com>
2020-10-09 18:16:27 +0300

    Update MicroCliCommand.java

--- MicroCli/src/main/java/io/hashimati/microcli/MicroCliCommand.java
+++ MicroCli/src/main/java/io/hashimati/microcli/MicroCliCommand.java
@@ -1,11 +1,13 @@
 package io.hashimati.microcli;
-
+/**
+ * @author Ahmed Al Hashmi
+ */
 import io.hashimati.microcli.commands.*;
 import io.micronaut.configuration.picocli.PicocliRunner;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 
-@Command(name = "MicroCli", description = "...",
+@Command(name = "MicroCli", description = "A simple tool to generate Micronaut Components",
         mixinStandardHelpOptions = true, subcommands = {ConfigureCommand.class, CreateEntityCommand.class, CreateEnumCommand.class, CreateRelationCommand.class, DeleteEntityCommand.class, DeleteAttributeCommand.class,AddAttributeCommand.class, CreateControllerCommand.class, CreateSingletonCommand.class, CreateJobCommand.class, CreateClientCommand.class, CreateKafkaClientCommand.class, CreateKafkaListenerCommand.class, CreateRabbitMQClientCommand.class, CreateRabbitMQListenerCommand.class, CreateWebsocketClientCommand.class, CreateWebsocketServerCommand.class, CreateNatsClientCommand.class, CreateNatsListenerCommand.class, EnableJWTSecurityCommand.class, CreateGcpPubSubClientCommand.class, CreateGcpPubSubListenerCommand.class})
 public class MicroCliCommand implements Runnable {
 
