46fa10c6de05ff6b9b367e4956a34c6db221e22f
Ahmed Al Hashmi <hashimati.ahmed@gmail.com>
2021-08-30 07:30:04 +0300

    To Support Banners

---
+++ MicroCli/src/main/java/io/hashimati/microcli/utils/AsciiUtils.java
@@ -0,0 +1,14 @@
+package io.hashimati.microcli.utils;
+
+import io.leego.banana.BananaUtils;
+import io.leego.banana.Font;
+
+public class AsciiUtils {
+
+
+    public void foo(){
+        
+        BananaUtils.bananaify("Hello, Banana!", Font.ALLIGATOR2);
+
+    }
+}
