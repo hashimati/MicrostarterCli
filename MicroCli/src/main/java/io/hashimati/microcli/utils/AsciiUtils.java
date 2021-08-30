7306dc4396ba2726702bf5e8a733a3cde1e4f37f
Ahmed Al Hashmi <hashimati.ahmed@gmail.com>
2021-08-30 07:43:42 +0300

    Ascii Art

--- MicroCli/src/main/java/io/hashimati/microcli/utils/AsciiUtils.java
+++ MicroCli/src/main/java/io/hashimati/microcli/utils/AsciiUtils.java
@@ -3,12 +3,25 @@ package io.hashimati.microcli.utils;
 import io.leego.banana.BananaUtils;
 import io.leego.banana.Font;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class AsciiUtils {
 
 
-    public void foo(){
-        
-        BananaUtils.bananaify("Hello, Banana!", Font.ALLIGATOR2);
 
+    public static List<String> getFontNames(){
+
+        return BananaUtils.fonts()
+                .stream()
+                .map(x-> x.getName())
+                .collect(Collectors.toList());
+    }
+    public String getBanner(String name, String fontName)
+    {
+        return BananaUtils.bananaify(name, Font.get(fontName));
+    }
+    public void foo(){
+        BananaUtils.bananaify("Hello, Banana!", Font.ALLIGATOR2);
     }
 }
