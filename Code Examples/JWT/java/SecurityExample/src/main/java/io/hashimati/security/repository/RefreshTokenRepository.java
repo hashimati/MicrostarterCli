e2a968c130fd5e9e23a9c2f92acd31b1eae3f726
Ahmed Al Hashmi <hashimati.ahmed@gmail.com>
2021-09-16 07:32:45 +0300

    Refresh token

--- Code Examples/JWT/java/SecurityExample/src/main/java/io/hashimati/security/repository/RefreshTokenRepository.java
+++ Code Examples/JWT/java/SecurityExample/src/main/java/io/hashimati/security/repository/RefreshTokenRepository.java
@@ -2,18 +2,29 @@ package io.hashimati.security.repository;
 
 
 import io.hashimati.security.domains.LoginStatus;
+import io.hashimati.security.domains.RefreshToken;
 import io.hashimati.security.domains.User;
+import io.micronaut.core.annotation.NonNull;
 import io.micronaut.data.jdbc.annotation.JdbcRepository;
 import io.micronaut.data.model.query.builder.sql.Dialect;
 import io.micronaut.data.repository.CrudRepository;
 
+import javax.transaction.Transactional;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 @JdbcRepository(dialect = Dialect.H2)
-public interface RefreshTokenRepository extends CrudRepository<User, Long> {
-    public User findByUsername(String username);
-    public List<User> findAll();
-    public boolean existsByUsername(String username);
-    public Long updateByUsername(String username, LoginStatus lastLoginStatus, Date lastTimeTryToLogin);
+public interface RefreshTokenRepository extends CrudRepository<RefreshToken, Long> {
+    @Transactional
+    RefreshToken save(@NonNull @NotBlank String username,
+                            @NonNull @NotBlank String refreshToken,
+                            @NonNull @NotNull Boolean revoked);
+
+    Optional<RefreshToken> findByRefreshToken(@NonNull @NotBlank String refreshToken);
+
+    long updateByUsername(@NonNull @NotBlank String username,
+                          boolean revoked);
 }
--- Code Examples/JWT/java/SecurityExample/src/main/java/io/hashimati/security/services/CustomRefreshTokenPersistence.java
+++ Code Examples/JWT/java/SecurityExample/src/main/java/io/hashimati/security/services/CustomRefreshTokenPersistence.java
@@ -1,21 +1,55 @@
 package io.hashimati.security.services;
 
 
+import io.hashimati.security.domains.RefreshToken;
+import io.hashimati.security.repository.RefreshTokenRepository;
 import io.micronaut.security.authentication.Authentication;
+import io.micronaut.security.errors.OauthErrorResponseException;
 import io.micronaut.security.token.event.RefreshTokenGeneratedEvent;
 import io.micronaut.security.token.refresh.RefreshTokenPersistence;
 import jakarta.inject.Singleton;
 import org.reactivestreams.Publisher;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.FluxSink;
+
+import java.util.Optional;
+
+import static io.micronaut.security.errors.IssuingAnAccessTokenErrorCode.INVALID_GRANT;
 
 @Singleton
 public class CustomRefreshTokenPersistence implements RefreshTokenPersistence {
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    public CustomRefreshTokenPersistence(RefreshTokenRepository refreshTokenRepository) {
+        this.refreshTokenRepository = refreshTokenRepository;
+    }
+
     @Override
     public void persistToken(RefreshTokenGeneratedEvent event) {
-
+        if (event != null &&
+                event.getRefreshToken() != null &&
+                event.getAuthentication() != null &&
+                event.getAuthentication().getName() != null) {
+            String payload = event.getRefreshToken();
+            refreshTokenRepository.save(event.getAuthentication().getName(), payload, false);
+        }
     }
 
     @Override
     public Publisher<Authentication> getAuthentication(String refreshToken) {
-        return null;
+        return Flux.create(emitter -> {
+            Optional<RefreshToken> tokenOpt = refreshTokenRepository.findByRefreshToken(refreshToken);
+            if (tokenOpt.isPresent()) {
+                RefreshToken token = tokenOpt.get();
+                if (token.getRevoked()) {
+                    emitter.error(new OauthErrorResponseException(INVALID_GRANT, "refresh token revoked", null));
+                } else {
+                    emitter.next(Authentication.build(token.getUsername()));
+                    emitter.complete();
+                }
+            } else {
+                emitter.error(new OauthErrorResponseException(INVALID_GRANT, "refresh token not found", null));
+            }
+        }, FluxSink.OverflowStrategy.ERROR);
     }
 }
