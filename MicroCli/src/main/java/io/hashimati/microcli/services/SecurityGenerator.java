2d7147e227fab28542f9141c8a0cdf1b99fe2306
Ahmed Al Hashmi <hashimati.ahmed@gmail.com>
2021-09-30 08:03:17 +0300

    Generating Security Files

--- MicroCli/src/main/java/io/hashimati/microcli/services/SecurityGenerator.java
+++ MicroCli/src/main/java/io/hashimati/microcli/services/SecurityGenerator.java
@@ -4,13 +4,12 @@ package io.hashimati.microcli.services;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import io.hashimati.microcli.domains.ConfigurationInfo;
 import io.hashimati.microcli.utils.GeneratorUtils;
-import io.micronaut.core.io.ResourceResolver;
-import io.micronaut.core.io.scan.ClassPathResourceLoader;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -25,32 +24,50 @@ public class SecurityGenerator {
 
 
 
-    public void generateSecurityFiles(String strategy, ArrayList<String> roles) throws FileNotFoundException, JsonProcessingException {
+    public void generateSecurityFiles(String strategy, ArrayList<String> roles) throws IOException {
         ConfigurationInfo configurationInfo = ConfigurationInfo.fromFile(new File(ConfigurationInfo.getConfigurationFileName()));
 
-        ClassPathResourceLoader loader = new ResourceResolver().getLoader(ClassPathResourceLoader.class).get();
-        System.out.println(loader.getResource("classpath:micronaut/security").get());
+        auxGenerateSecurityFiles(strategy, roles, templatesService.getSecurityTemplates(), configurationInfo);
 
+
+        auxGenerateSecurityFiles(strategy, roles, templatesService.getSecurityControllerTemplates(), configurationInfo);
+        auxGenerateSecurityFiles(strategy, roles, templatesService.getSecurityDomainsTemplates(), configurationInfo);
+        auxGenerateSecurityFiles(strategy, roles, templatesService.getSecurityRepositoryTemplates(), configurationInfo);
+        auxGenerateSecurityFiles(strategy, roles, templatesService.getSecurityServicesTemplates(), configurationInfo);
+        auxGenerateSecurityFiles(strategy, roles, templatesService.getSecurityUtilsTemplates(), configurationInfo);
+
+        //System.out.println(templatesService.loadTemplateContent("micronaut/security/java/JDBC/domains/LoginEvent.java"));
+    }
+
+
+    public void auxGenerateSecurityFiles(String strategy, ArrayList<String> roles, HashMap<String, String> templates, ConfigurationInfo configurationInfo) throws IOException {
         String db = configurationInfo.getDataBackendRun(),
                 lang = configurationInfo.getProjectInfo().getSourceLanguage(),
-                ext = lang.equalsIgnoreCase(KOTLIN_LANG)? "kt":lang;
-        for(String key : templatesService.getSecurityTemplates().keySet()){
-            String path = templatesService.getSecurityTemplates().get(key);
-            path = GeneratorUtils.generateFromTemplate(path, new HashMap<String, String>(){{
+                ext = GeneratorUtils.getSourceFileExtension(lang);
+        for (String key : templates.keySet()) {
+            String path = templates.get(key);
+            path = GeneratorUtils.generateFromTemplate(path, new HashMap<String, String>() {{
                 put("db", db);
                 put("lang", lang);
                 put("ext", ext);
             }});
+
+            String filePath = System.getProperty("user.dir")+"/src/main/"+ lang + "/"+ GeneratorUtils.packageToPath(configurationInfo.getProjectInfo().getDefaultPackage())
+                    + path.substring(path.indexOf("/security")).replace("/"+ lang + "/" + db, "" );
             String template = templatesService.loadTemplateContent(path);
 
             String securityPackage = new StringBuilder().append(configurationInfo.getProjectInfo().getDefaultPackage()).append(".security").toString();
-            String fileContent = GeneratorUtils.generateFromTemplate(template, new HashMap<String, String>(){{
+
+            String fileContent = GeneratorUtils.generateFromTemplate(template, new HashMap<String, String>() {{
                 put("securityPackage", securityPackage);
                 put("roles", "");
             }});
 
+            System.out.println("-----");
+            System.out.println(filePath);
             System.out.println(fileContent);
+            System.out.println("-----");
+            GeneratorUtils.createFile(filePath, fileContent);
         }
-        //System.out.println(templatesService.loadTemplateContent("micronaut/security/java/JDBC/domains/LoginEvent.java"));
     }
 }
