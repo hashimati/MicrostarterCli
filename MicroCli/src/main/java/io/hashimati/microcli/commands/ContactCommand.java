85761bf41e9be87a0095d9e117fb5d082cb66575
Ahmed Al Hashmi <hashimati.ahmed@gmail.com>
2021-10-20 15:05:55 +0300

    Update ContactCommand.java

--- MicroCli/src/main/java/io/hashimati/microcli/commands/ContactCommand.java
+++ MicroCli/src/main/java/io/hashimati/microcli/commands/ContactCommand.java
@@ -10,11 +10,9 @@ import java.util.concurrent.Callable;
 
 @Command(name = "contact", aliases = {"mail", "email"}, description = "To contact the developer.")
 public class ContactCommand implements Callable<Integer> {
-
     @Override
     public Integer call() throws Exception {
         Desktop.getDesktop().mail( URI.create("mailto:hashimati.ahmed@gmail.com"));
-
         return 0;
     }
 }
