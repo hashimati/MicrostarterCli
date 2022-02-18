589ddfa9ef6f694a6469b66477bd8edc2515e97c
Ahmed Al Hashmi <hashimati.ahmed@gmail.com>
2022-02-19 00:33:58 +0300

    disable sslCommand

--- MicroCli/src/main/java/io/hashimati/microcli/commands/SecurityCommand.java
+++ MicroCli/src/main/java/io/hashimati/microcli/commands/SecurityCommand.java
@@ -28,7 +28,7 @@ import static de.codeshelf.consoleui.elements.ConfirmChoice.ConfirmationValue.NO
 import static org.fusesource.jansi.Ansi.ansi;
 
 @Command(name = "security", description = {"To enable Security", "This command will generate the security files based the configuration, and selected security mechanism."},
-subcommands = SSLCommand.class)
+)//subcommands = SSLCommand.class)
 public class SecurityCommand implements Callable<Integer> {
 
     @Inject
