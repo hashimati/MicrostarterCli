f93e5d27f1f3ae659c722cbf11a7390404a9bcee
Ahmed Al Hashmi <hashimati.ahmed@gmail.com>
2022-07-08 15:53:45 +0300

    Get Patterns

--- MicroCLIDomainLanguage/src/main/java/io/hashimati/utils/PatternUtils.java
+++ MicroCLIDomainLanguage/src/main/java/io/hashimati/utils/PatternUtils.java
@@ -1,12 +1,18 @@
 package io.hashimati.utils;
 
-import java.util.regex.Matcher;
+import java.util.List;
+import java.util.regex.MatchResult;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class PatternUtils {
 
-    public static String getPatternedFromtext(String pattern, String text){
+    public static List<String> getPatternsFromtext(String pattern, String text){
 
-        return null;
+        return Pattern.compile(pattern)
+                .matcher(text)
+                .results()
+                .map(MatchResult::group)
+                .collect(Collectors.toList());
     }
 }
